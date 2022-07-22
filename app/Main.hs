953a3894f44f41c17f0fdd6c9db2e3240940c08c
Evan Clough <evanclough99@gmail.com>
2022-07-21 20:28:49 -0400

    made basic command parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Prelude hiding (lookup)
+import Data.Map 
+
+help :: [String] -> IO ()
+help _ = putStrLn "the commands are: "
+
+commandMap :: Map String ([String] -> IO ())
+commandMap = fromList [("help", help)]
+
+parseInput :: String -> IO ()
+parseInput inp = case isValidCmd of Nothing -> putStrLn $ "invalid command: " ++ (head $ words inp)
+                                    Just f -> f $ tail $ words inp
+                                    where isValidCmd = lookup (head $ words inp) commandMap
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =  do
+            inp <- getLine
+            parseInput inp
+            main
